c212d34ab76742f269ae89029dde9e3cd09fa7a0
Raevens Robbe <robbe.raevens@hotmail.com>
2021-10-20 15:50:20 +0200

    setup sveltekit and tailwind

---
+++ Frontend/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<main>
+    <slot></slot>
+</main>
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
---
+++ Frontend/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <title>MCT Airlines</title>
+</svelte:head>
+
+<h1 class=" text-2xl font-bold">Homepage</h1>
