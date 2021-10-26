60fe30b6987b82b38aa8c95ab14ffb5781c3d353
Raevens Robbe <robbe.raevens@hotmail.com>
2021-10-26 15:57:32 +0200

    overflow change

--- Frontend/src/components/PopularDestinationCard.svelte
+++ Frontend/src/components/PopularDestinationCard.svelte
@@ -16,7 +16,7 @@
     <span class="h-2 w-3/4 bg-forest-green block"></span>
     <div class="w-full flex justify-between items-center p-2 bg-white">
         <div>
-            <h1 class="font-bold text-lg text-dim-gray mb-1 whitespace-nowrap overflow-hidden">{ popularDestination.Dest[0].Name }, { popularDestination.Name}</h1>
+            <h1 class="font-bold text-lg text-dim-gray mb-1 whitespace-nowrap">{ popularDestination.Dest[0].Name }, { popularDestination.Name}</h1>
             <div class="flex">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" viewBox="0 0 28.48 35.065">
                     <path id="Subtraction_1" data-name="Subtraction 1" d="M708.24-1370.935h0a70.014,70.014,0,0,1-10.657-11.613c-2.377-3.465-3.582-6.564-3.582-9.211a14.147,14.147,0,0,1,4.171-10.069A14.147,14.147,0,0,1,708.24-1406a14.147,14.147,0,0,1,10.069,4.171,14.147,14.147,0,0,1,4.171,10.069c0,2.669-1.205,5.79-3.582,9.278a65.6,65.6,0,0,1-10.657,11.547ZM708-1397a5.006,5.006,0,0,0-5,5,5.006,5.006,0,0,0,5,5,5.006,5.006,0,0,0,5-5A5.005,5.005,0,0,0,708-1397Z" transform="translate(-694 1406)" fill="#fc5757"/>
