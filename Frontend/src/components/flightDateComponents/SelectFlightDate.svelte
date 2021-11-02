91dcb2790fbcb7f832b4d4075bdc776a982be62b
Raevens Robbe <robbe.raevens@hotmail.com>
2021-11-02 18:57:32 +0100

    select date design made + active state

--- Frontend/src/components/flightDateComponents/SelectFlightDate.svelte
+++ Frontend/src/components/flightDateComponents/SelectFlightDate.svelte
@@ -6,11 +6,12 @@ import { onMount } from "svelte";
   let givenflights;
   let storageDate = "2021-11-30"
   let position;
-  let flights = [];
+  let flights = new Array();
+  let loaded = false;
 
   onMount(async()=>{
     givenflights = await get("http://localhost:3001/api/v1/flight/flightInfo/3/1")
-    console.log(givenflights)
+    // console.log(givenflights)
 
     let i = 0;
 
@@ -23,29 +24,49 @@ import { onMount } from "svelte";
       i++
     })
 
-    for(let j = -3; j <3; j++){
+    i=0
+    for(let j = -3; j <4; j++){
       if(j===0){
-        console.log(position)
+        // console.log(position)
         let flightInfo = await get(`http://localhost:3001/api/v1/flight/${givenflights[position].FlightId}`)
         flights.push(flightInfo)
       } else{
-        console.log(position+j)
+        // console.log(position+j)
         let flightInfo = await get(`http://localhost:3001/api/v1/flight/${givenflights[position+j].FlightId}`)
         flights.push(flightInfo)
       }
+      i++
     }
     console.log(flights)
+    loaded = true
   })
+
+  function calculatedayname(date) {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date(date);
+    return days[d.getDay()]
+  }
 </script>
 
-<main class="grid grid-cols-7">
-{#if flights}
-  <p>total flights: {flights.length}</p>
+<main class="grid grid-cols-7 border-r-0 items-end">
+{#if loaded}
   {#each flights as flight}
-    <div>
-      <p>€{flight.Price}</p>
-      <p>test</p>
-    </div>
+    <label key={flight.FlightId}>
+      <input
+      type="radio"
+      name="city"
+      id={flight.FlightId}
+      value={flight.Date}
+      class="peer hidden"
+      />
+      <div class="bg-white border-forest-green peer-checked:border-t-8 align-bottom">
+        <div class="bg-white border-r-1 border-l-1 flex flex-col justify-items-end  text-center shadow-md justify-end">
+          <p class="text-forest-green font-bold m-2 text-xl">{calculatedayname(flight.Date.split("T")[0])}</p>
+          <p class="">{flight.Date.split("T")[0]}</p>
+          <p class="text-cyprus-green font-bold m-2 text-xl">€{flight.Price}</p>
+        </div>
+      </div>
+    </label>
   {/each}
 {:else}
   <p>loading</p>
