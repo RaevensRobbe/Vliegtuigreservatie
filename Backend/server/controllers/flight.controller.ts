39f5a8fc96df4aee782d24311d86f444e9981c6a
Raevens Robbe <robbe.raevens@hotmail.com>
2021-11-02 15:53:54 +0100

    added dummy data

--- Backend/server/controllers/flight.controller.ts
+++ Backend/server/controllers/flight.controller.ts
@@ -1,56 +1,72 @@
-import { Request, Response, NextFunction, Router } from 'express';
-import { Flight } from "../entities/flight"; 
-import { CrudController, IController, ICrudController } from './crud.controller';
+import { Request, Response, NextFunction, Router } from 'express'
+import { Flight } from '../entities/flight'
+import { CrudController, IController, ICrudController } from './crud.controller'
 
 /**
  * The interface to use for every Flight Controller.
  */
- export interface IFlightController extends ICrudController, IController  {
-     test: string;
+export interface IFlightController extends ICrudController, IController {
+  test: string
 }
 
-export class FlightController extends CrudController<Flight> implements IFlightController {
-    public router = Router();
-    public test = 'OK';
+export class FlightController
+  extends CrudController<Flight>
+  implements IFlightController
+{
+  public router = Router()
+  public test = 'OK'
 
-    constructor() {
-        super(Flight); // Initialize the parent constructor
+  constructor() {
+    super(Flight) // Initialize the parent constructor
 
-        this.router.get('/all', this.all);
-        this.router.get('/:id', this.one);
-        this.router.post('', this.save);
-        this.router.get('/flightInfo/:Sid/:Did',this.flightInfo)
-        this.router.get('/seats/:id', this.seats)
-    }
+    this.router.get('/all', this.all)
+    this.router.get('/:id', this.one)
+    this.router.post('', this.save)
+    this.router.get('/flightInfo/:Sid/:Did', this.flightInfo)
+    this.router.get('/seats/:id', this.seats)
+  }
 
-    flightInfo = async (request: Request, response: Response, next: NextFunction) => {
-        const startID = request.params.Sid;
-        const destinationID = request.params.Did;
+  flightInfo = async (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) => {
+    const startID = request.params.Sid
+    const destinationID = request.params.Did
 
-        console.log(startID)
-        console.log(destinationID)
+    console.log(startID)
+    console.log(destinationID)
 
-        const data = await this.repository.createQueryBuilder("f")
-        .select(["f.FlightId","f.Date","f.StartId","f.DestinationId","d.Name","s.Name"])
-        .innerJoin("f.Destination", "d")
-        .innerJoin("f.Start", "s")
-        .where("f.StartId = :id",{id:startID})
-        .andWhere("f.DestinationId = :id2",{id2:destinationID})
-        .getOne();
-        response.send(data);
-    }
+    const data = await this.repository
+      .createQueryBuilder('f')
+      .select([
+        'f.FlightId',
+        'f.Date',
+        'f.StartId',
+        'f.DestinationId',
+        'd.Name',
+        's.Name',
+      ])
+      .innerJoin('f.Destination', 'd')
+      .innerJoin('f.Start', 's')
+      .where('f.StartId = :id', { id: startID })
+      .andWhere('f.DestinationId = :id2', { id2: destinationID })
+      .getMany()
+    response.send(data)
+  }
 
-    seats = async (request: Request, response: Response, next: NextFunction) => {
-        const flightID = request.params.id;
+  seats = async (request: Request, response: Response, next: NextFunction) => {
+    const flightID = request.params.id
 
-        console.log(flightID)
+    console.log(flightID)
 
-        const data = await this.repository.createQueryBuilder("f")
-        .select(["f.FlightId","p.EconomySeats","p.BusinessSeats","t.Seat"])
-        .innerJoin("f.Plane", "p")
-        .innerJoin("f.Ticket", "t")
-        .where("f.FlightId = :id",{id:flightID})
-        .getMany();
-        response.send(data);
-    }
+    const data = await this.repository
+      .createQueryBuilder('f')
+      .select(['f.FlightId', 'p.EconomySeats', 'p.BusinessSeats', 't.Seat'])
+      .innerJoin('f.Plane', 'p')
+      .innerJoin('f.Ticket', 't')
+      .where('f.FlightId = :id', { id: flightID })
+      .getMany()
+    response.send(data)
+  }
 }
