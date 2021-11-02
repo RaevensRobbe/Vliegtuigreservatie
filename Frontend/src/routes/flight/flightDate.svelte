0ba7846f0871b985ac769bc48edb8eea21f596af
Raevens Robbe <robbe.raevens@hotmail.com>
2021-11-02 11:57:11 +0100

    created basics flightdate page

--- Frontend/src/routes/flight/flightDate.svelte
+++ Frontend/src/routes/flight/flightDate.svelte
@@ -1,6 +1,29 @@
 <script>
+  import AppHeader from './../../components/AppHeader.svelte'
+  import AppFooter from './../../components/AppFooter.svelte'
+  import FlightSearch from './../../components/FlightSearch.svelte'
+  import Intertitle from './../../components/Intertitle.svelte'
+  import { FlightStore } from './../../stores/flightStore'
+
+  let name = 'Raevens Robbe'
+
+  let flight = $FlightStore
 </script>
 
-<body>
-  <p>flightDate</p>
-</body>
+<svelte:head>
+  <title>MCT Airlines - Flight Date</title>
+</svelte:head>
+
+<AppHeader {name} />
+
+<section class=" bg-image-mainpage bg-cover w-full h-auto bg-bottom py-16">
+  <FlightSearch />
+</section>
+
+<section class="p-4 px-6 align-start">
+  <Intertitle
+    titleName={flight.departureLocation + ' - ' + flight.destinationLocation}
+  />
+</section>
+
+<AppFooter />
