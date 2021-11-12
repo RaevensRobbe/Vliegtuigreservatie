985a2b68bf3cf23d9277640ffadf3834ddf606d3
Raevens Robbe <robbe.raevens@hotmail.com>
2021-11-12 17:36:36 +0100

    layout flight time created

--- Frontend/src/components/flightOverviewComponents/FligthDateTime.svelte
+++ Frontend/src/components/flightOverviewComponents/FligthDateTime.svelte
@@ -1,2 +1,33 @@
 <script lang="ts">
 </script>
+
+<section class="flex flex-col my-4">
+  <h1 class="font-bold text-forest-green text-lg">09 October</h1>
+  <div class="my-2">
+    <p class="font-bold text-cyprus-green">08:20</p>
+    <p>Brussel-Zaventem</p>
+  </div>
+  <div class="self-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="fill-current text-cyprus-green h-3 md:h-4"
+      viewBox="0 0 44 22.458"
+    >
+      <g
+        id="Flight_icon"
+        data-name="Flight icon"
+        transform="translate(-829 -1009.771)"
+      >
+        <path
+          id="bxs-plane-take-off"
+          d="M46.813,14.674a3.616,3.616,0,0,0-4.572-2.288L32.195,15.737,14.229,9,9.737,11.246l13.475,8.983L14.229,24.72,5.246,20.229,3,22.475l8.983,8.983,32.67-12.251a3.619,3.619,0,0,0,2.16-4.532Z"
+          transform="translate(826 1000.771)"
+        />
+      </g>
+    </svg>
+  </div>
+  <div class="my-2">
+    <p class="font-bold text-cyprus-green">09:50</p>
+    <p>Milan Malpensa</p>
+  </div>
+</section>
--- Frontend/src/routes/flight/flightOverview.svelte
+++ Frontend/src/routes/flight/flightOverview.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+  import FligthDateTime from './../../components/flightOverviewComponents/FligthDateTime.svelte'
   import Intertitle from './../../components/Intertitle.svelte'
 
   //@ts-nocheck
@@ -38,5 +40,42 @@
 
 <section class="p-4 px-6 align-start">
   <Intertitle titleName="Overview" />
-  <div class="p-4 grid grid-cols-2" />
+  <div class="p-4 grid grid-rows-2 bg-white shadow-md">
+    <div class="grid grid-cols-2 justify-center text-center">
+      <div>
+        <div class="flex justify-center ">
+          <svg
+            class="text-forest-green h-8 w-8 mx-2"
+            id="flight_takeoff_black_24dp"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+          >
+            <path
+              id="Path_13"
+              data-name="Path 13"
+              d="M0,0H32V32H0Z"
+              fill="none"
+            />
+            <path
+              id="Path_14"
+              data-name="Path 14"
+              d="M2.76,26.082h26.5V29H2.76ZM30.05,12.426a2.079,2.079,0,0,0-2.566-1.547l-7.4,2.072L10.458,3.57l-2.691.744L13.54,14.775l-6.931,1.94L3.862,14.469l-2.022.569,3.612,6.551,23.106-6.463a2.234,2.234,0,0,0,1.492-2.7Z"
+              transform="translate(0 0)"
+              fill="#008066"
+            />
+          </svg>
+
+          <p class="font-bold text-2xl text-forest-green">Brussel - Milan</p>
+        </div>
+        <FligthDateTime />
+        <p>Seat price section</p>
+      </div>
+      <div>
+        <p>Milan - Brussel</p>
+      </div>
+    </div>
+    <div class="p-4">
+      <p class="text-2xl text-cyprus-green font-bold">Total: €898.36</p>
+    </div>
+  </div>
 </section>
