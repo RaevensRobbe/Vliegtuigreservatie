ce4d140872e9b05040a974b5fe17a58a0a1a77db
Jelle <jelle.demets@student.howest.be>
2021-11-17 12:02:07 +0100

    loading in passenger for picking seat

---
+++ Frontend/src/components/pickSeatsComponents/PassengersComponent.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let fN:string
+    export let lN:string
+    export let seatNr:string
+    console.log(`${fN}`)
+</script>
+
+<div class="flex flex-col">
+    <h1 class="font-bold text-lg text-forest-green">{fN} {lN}</h1>
+    <p>Select your seat</p>
+</div>
+
+<div class="flex justify-center">
+    <div class="w-12 h-12 bg-forest-green">
+        <h1
+        class="w-full flex h-full items-center justify-center text-white font-bold text-center text-xl"
+    >
+        {seatNr}
+    </h1>
+    </div>
+</div>
+
+<div class="flex justify-center">
+    <div class="w-12 h-12 bg-white border border-forest-green ">
+        <h1
+        class="w-full flex h-full items-center justify-center text-white font-bold text-center text-xl"
+        >
+            {seatNr}
+        </h1>
+    </div>
+</div>
