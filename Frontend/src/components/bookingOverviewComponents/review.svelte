24bd18296225921b50dcc16d0e1e498456b92627
Raevens Robbe <robbe.raevens@hotmail.com>
2021-11-28 13:56:51 +0100

    review score en review text binded

--- Frontend/src/components/bookingOverviewComponents/review.svelte
+++ Frontend/src/components/bookingOverviewComponents/review.svelte
@@ -1,18 +1,25 @@
-<script>
+<script lang="ts">
   import StarRating from './starRating.svelte'
-
-  let value = ''
+  let rating: number = 0
+  let reviewText: string = ''
+  function handleSubmit() {
+    console.log(rating)
+    console.log(reviewText)
+  }
 </script>
 
-<form class="my-4 mx-6 flex flex-col bg-white shadow-md p-4">
+<form
+  class="my-4 mx-6 flex flex-col bg-white shadow-md p-4"
+  on:submit|preventDefault={handleSubmit}
+>
   <div>
     <h1 class="text-xl font-bold">Overal Rating</h1>
-    <StarRating />
+    <StarRating bind:ratingScore={rating} />
   </div>
   <div>
     <h1 class="text-xl font-bold">Write your review</h1>
     <textarea
-      {value}
+      bind:value={reviewText}
       class="w-full h-36 p-2 bg-ghost-white border-1 border-current my-4"
     />
   </div>
