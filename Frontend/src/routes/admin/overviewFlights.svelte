3f3fad7ef42ea03b1ebc5706950daecc3d3ef870
Raevens Robbe <robbe.raevens@hotmail.com>
2021-12-01 15:12:19 +0100

    general layout created

---
+++ Frontend/src/routes/admin/overviewFlights.svelte
@@ -0,0 +1,19 @@
+<script lnag="ts">
+  import Intertitle from '../../components/Intertitle.svelte'
+</script>
+
+<body>
+  <section class="bg-image-mainpage bg-cover w-full h-auto bg-bottom py-36">
+    <p>search flight number</p>
+  </section>
+  <section class="m-4 px-6">
+    <button
+      class="flex p-4 justify-center items-center font-bold text-xl text-white bg-forest-green rounded-xl hover:bg-cyprus-green"
+    >
+      Add a flight
+    </button>
+  </section>
+  <section class="m-4 px-6">
+    <Intertitle titleName="All upcoming flights" />
+  </section>
+</body>
