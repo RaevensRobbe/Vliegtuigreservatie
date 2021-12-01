a23516704375f672977e6bfbb5e338bd064d5c25
Raevens Robbe <robbe.raevens@hotmail.com>
2021-12-01 16:58:24 +0100

    search flight component made

---
+++ Frontend/src/components/adminComponents/searchFlightNumberComponent.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let flightNumber: string
+  let tempFlightNumber: string
+
+  function handleSubmit() {
+    flightNumber = flightNumber
+    console.log(flightNumber)
+  }
+</script>
+
+<form
+  class="flex justify-center w-3/4 lg:w-3/5 xl:w-1/2 mx-auto"
+  on:submit|preventDefault={handleSubmit}
+>
+  <div class="bg-white rounded-l-xl col-span-2 flex">
+    <input
+      type="text"
+      placeholder="Search flight number"
+      class="p-4 rounded-l-xl"
+    />
+  </div>
+  <button
+    class="bg-forest-green text-white font-bold text-2xl p-4 rounded-r-xl hover:bg-cyprus-green cursor-pointer"
+  >
+    Search
+  </button>
+</form>
--- Frontend/src/routes/admin/overviewFlights.svelte
+++ Frontend/src/routes/admin/overviewFlights.svelte
@@ -1,10 +1,21 @@
-<script lnag="ts">
+<script lang="ts">
   import Intertitle from '../../components/Intertitle.svelte'
+  import SearchFlightNumberComponent from '../../components/adminComponents/SearchFlightNumberComponent.svelte'
+  import { onMount } from 'svelte'
+  import { get } from '../../utils/useApi'
+  let flightNumber: string = null
+
+  let flights: any = []
+  let flightsLoaded: boolean = false
+  onMount(async () => {
+    flights = await get('http://localhost:3001/api/v1/flight/all')
+    flightsLoaded = true
+  })
 </script>
 
 <body>
   <section class="bg-image-mainpage bg-cover w-full h-auto bg-bottom py-36">
-    <p>search flight number</p>
+    <SearchFlightNumberComponent {flightNumber} />
   </section>
   <section class="m-4 px-6">
     <button
@@ -15,5 +26,8 @@
   </section>
   <section class="m-4 px-6">
     <Intertitle titleName="All upcoming flights" />
+    <div>
+      <p>flights komen hier</p>
+    </div>
   </section>
 </body>
