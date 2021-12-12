76ebfc7c9e67767ac6c3dd68a420be3367cc425c
Jelle <jelle.demets@student.howest.be>
2021-12-12 15:13:58 +0100

    fixed change pw

--- Frontend/src/routes/user/accountInfo.svelte
+++ Frontend/src/routes/user/accountInfo.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { emailValidator, requiredValidator } from '../../utils/inputValidator'
+  import { emailValidator, requiredValidator, confirmValidator } from '../../utils/inputValidator'
   import Intertitle from '../../components/Intertitle.svelte'
   import { onMount } from 'svelte'
   import { get, post } from '../../utils/useApi'
   import authStore from '../../stores/authStore'
-  import { updateCurrentUser, updateEmail, updateProfile } from '@firebase/auth'
+  import { updateCurrentUser, updateEmail, updatePassword, updateProfile } from '@firebase/auth'
   import { getAuth } from 'firebase/auth'
   import { put } from '../../utils/useApi'
   import { goto } from '$app/navigation'
@@ -15,6 +15,9 @@
   let errors: any = {}
   let succes: any = {}
   let userdata: any = {}
+  let pw:string
+  let cpw:string
+  let test:string = ''
 
   onMount(async () => {
     console.log($authStore.user.uid)
@@ -22,7 +25,7 @@
       `http://localhost:3001/api/v1/user/data/${$authStore.user.uid}`,
     )
     userdata = getData
-    //console.log(userdata)
+    console.log(userdata)
   })
 
   async function updateUser(data) {
@@ -73,9 +76,11 @@
       requiredValidator(userdata.Firstname) &&
       requiredValidator(userdata.Lastname)
     ) {
+      console.log(errors)
       errors.firstname = 'Firstname is required'
       errors.lastname = 'Lastname is required'
       errors.email = 'Email is required'
+      
       return
     }
 
@@ -99,7 +104,54 @@
       } else errors.email = ''
     }
 
+    
     changeUserData()
+  }
+
+  const changePassword = async () => {
+    updatePassword(auth.currentUser, pw).then(() => {
+      //ROBBE FIX LOADING DINGKIE
+      console.log('yeey')
+    }).catch((error) => {
+      if(error.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).')
+        errors.updatepw ='Password to weak'
+      else{
+        errors.updatepw = 'Something went wrong'
+      }
+      console.log(error.message)
+      
+    })  
+  }
+
+  const onSubmitPassword = () => {
+    console.log(pw)
+    if(requiredValidator(pw) && requiredValidator(cpw)){
+      console.log('test2')
+      errors.newPw = 'New password is required'
+      errors.cNewPw = 'Confirm password is required'
+      return 
+    }else{
+      errors.newPw = ''
+      errors.cNewPw = ''
+    }
+    if(!confirmValidator(pw, cpw)){
+      errors.cNewPw = 'Passwords dont match'
+      return 
+    }else{
+      errors.cNewPw = ''
+    }
+
+    if (requiredValidator(pw)) {
+      errors.newPw = 'Password is required'
+      return
+    } else errors.newPw = ''
+
+    if (requiredValidator(cpw)) {
+      errors.cNewPw = 'Confirm password'
+      return
+    } else errors.cNewPw = ''
+
+    changePassword()
   }
 
   function goBack() {
@@ -150,7 +202,6 @@
               class=" w-full py-1 focus:outline-none focus:ring focus:ring-forest-green text-sm md:text-md"
             />
           </div>
-
           {#if errors.firstname}
             <p class="text-red-600 -mt-2 mb-2">{errors.firstname}</p>
           {/if}
@@ -196,8 +247,8 @@
               class=" w-full py-1 focus:outline-none focus:ring focus:ring-forest-green text-sm md:text-md"
             />
           </div>
-          {#if errors.email}
-            <p class="text-red-600 -mt-2 mb-2">{errors.email}</p>
+          {#if errors.picture}
+            <p class="text-red-600 -mt-2 mb-2">{errors.picture}</p>
           {/if}
         </div>
       </div>
@@ -220,4 +271,58 @@
       </div>
     </form>
   </section>
+
+  <section class="flex justify-center self-center">
+    <form
+      on:submit|preventDefault={onSubmitPassword} 
+      class="w-4/5 sm:w-3/5 md:w-4/5 lg:w-3/5 bg-white p-8 flex flex-col shadow-md"
+    >
+
+      <div class="flex flex-col">
+        <label for="password" class="mb-2"> New password </label>
+        <div class="border-b text-dim-gray mb-2 border-current">
+          <input
+            bind:value={pw}
+            id="password"
+            type="password"
+            class=" w-full py-1 focus:outline-none focus:ring focus:ring-forest-green text-sm md:text-md"
+          />
+        </div>
+        {#if errors.newPw}
+          <p class="text-red-600 -mt-2 mb-2">{errors.newPw}</p>
+        {/if}
+      </div>
+
+      <div class="flex flex-col">
+        <label for="cpassword" class="mb-2"> Confirm password </label>
+        <div class="border-b text-dim-gray mb-2 border-current">
+          <input
+            bind:value={cpw}
+            id="cpassword"
+            type="password"
+            class=" w-full py-1 focus:outline-none focus:ring focus:ring-forest-green text-sm md:text-md"
+          />
+        </div>
+        {#if errors.cNewPw}
+          <p class="text-red-600 -mt-2 mb-2">{errors.cNewPw}</p>
+        {/if}
+      </div>
+
+      <div class="flex justify-center mt-8">
+        <button
+          type="submit"
+          class="flex p-4 justify-center items-center font-bold text-2xl text-white bg-forest-green rounded-xl hover:bg-cyprus-green"
+        >
+          Change
+        </button>
+      </div>
+      <div class="flex justify-center">
+        {#if errors.updatepw}
+          <p class="text-red-600 -mt-2 mb-2">{errors.updatepw}</p>
+        {/if} 
+      </div>
+
+    </form>
+  </section>
+  
 </section>
